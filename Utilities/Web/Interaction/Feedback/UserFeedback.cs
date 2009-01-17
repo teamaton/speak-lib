c03da6537233d4faae77294dc1c9f6e696ba9b87
noackstefan <noackstefan@63a8470e-8155-0410-a357-b11873b86f21>
2009-01-17 14:43:14 +0000

    feedback, current development state
    git-svn-id: https://speak-lib.googlecode.com/svn/trunk@44 63a8470e-8155-0410-a357-b11873b86f21

---
+++ Utilities/Web/Interaction/Feedback/UserFeedback.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpeakFriend.Utilities.Web
+{
+    public class UserFeedback
+    {
+        public string Message { get; set; }
+        public bool IsSet { get; set; }
+        public bool Given { get; set; }
+        
+    }
+}
