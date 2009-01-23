37b3b388c8aee12179f039c77d4954f2f519ca07
RobertMischke <RobertMischke@63a8470e-8155-0410-a357-b11873b86f21>
2009-01-23 04:19:49 +0000

    - added missing ValidationSettings
    git-svn-id: https://speak-lib.googlecode.com/svn/trunk@55 63a8470e-8155-0410-a357-b11873b86f21

---
+++ SpeakLib.Utilities/Web/Interaction/Validation/ValidationSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpeakFriend.Utilities.Web
+{
+    public class ValidationSettings
+    {
+        /// <summary>
+        /// The css clas auf the "ValidationCallout" (The div beside an control, showing a user message)
+        /// </summary>
+        public string CssClass_Callout;
+        public string CssClass_CalloutHighlight;
+        public string ImgPath_CalloutWarning;
+    }
+}
