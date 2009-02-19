0648ecd09b2d3718f74964741edda516e3c48c9f
RobertMischke <RobertMischke@63a8470e-8155-0410-a357-b11873b86f21>
2009-02-19 20:37:41 +0000

    - minor
    git-svn-id: https://speak-lib.googlecode.com/svn/trunk@66 63a8470e-8155-0410-a357-b11873b86f21

---
+++ SpeakLib.Utilities/Web/Ajax/AjaxUtils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.UI;
+
+namespace SpeakFriend.Utilities.Web
+{
+    public class AjaxUtils
+    {
+        public static bool IsInPartialRendering(Control control)
+        {
+            foreach (var updatePanel in control.Controls.OfType<UpdatePanel>())
+            {
+                if (updatePanel.IsInPartialRendering)
+                    return true;
+            }
+            foreach (var container in control.Controls.OfType<Control>())
+            {
+                if (IsInPartialRendering(container))
+                    return true;
+            }
+            return false;
+        }
+    }
+}
