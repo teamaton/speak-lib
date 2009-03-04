0f32d194731b8b7a28211b9b859b48cad07a4b0e
RobertMischke <RobertMischke@63a8470e-8155-0410-a357-b11873b86f21>
2009-03-04 05:51:47 +0000

    - added: HtmlControlExtensionMethods
    git-svn-id: https://speak-lib.googlecode.com/svn/trunk@75 63a8470e-8155-0410-a357-b11873b86f21

---
+++ SpeakLib.Utilities/Web/Control/HtmlControlExtensionMethods.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.UI.HtmlControls;
+
+namespace SpeakFriend.Utilities.Web
+{
+    public static class HtmlControlExtensionMethods
+    {
+        /// <summary>
+        /// Returns the CSS with the specified class appended. Checks if the class exists before adding.
+        /// </summary>
+        public static void AddCssClass(this HtmlControl control, string classToAdd)
+        {
+            if (string.IsNullOrEmpty(control.Attributes["class"]))
+            {
+                control.Attributes["class"] = classToAdd;
+                return;
+            }
+
+            if (control.Attributes["class"].Equals(classToAdd)
+                || control.Attributes["class"].Contains(classToAdd + " ")
+                || control.Attributes["class"].Contains(" " + classToAdd))
+                return;
+
+            control.Attributes["class"] += " " + classToAdd;
+        }
+    }
+}
