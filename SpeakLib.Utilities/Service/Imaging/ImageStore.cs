008f789f1a8a381f5d4898dbb7dd184b9bc6daa8
noackstefan <noackstefan@63a8470e-8155-0410-a357-b11873b86f21>
2009-03-06 00:55:46 +0000

    image store
    git-svn-id: https://speak-lib.googlecode.com/svn/trunk@86 63a8470e-8155-0410-a357-b11873b86f21

---
+++ SpeakLib.Utilities/Service/Imaging/ImageStore.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace SpeakFriend.Utilities
+{
+    public class ImageStore
+    {
+        private readonly string _pathAbsolute;
+        private readonly string _pathRelative;
+
+        public ImageStore(string pathAbsolute, string pathRelative)
+        {
+            _pathAbsolute = pathAbsolute;
+            _pathRelative = pathRelative;
+        }
+
+        public void Store(string key, string sourcePath)
+        {
+            var image = Image.FromFile(sourcePath);
+            image.Save(GetPathAbsolute(key), ImageFormat.Png);
+        }
+  
+        public string Get(string key)
+        {
+            //ToDo: check if exists and if not, return default placeholder
+            return GetPathRelative(key);
+        }
+
+        private string GetPathAbsolute(string key)
+        {
+            return Path.Combine(_pathAbsolute, string.Format("{0}.png", key));
+        }
+
+        private string GetPathRelative(string key)
+        {
+            return Path.Combine(_pathRelative, string.Format("{0}.png", key));
+        }
+    }
+}
