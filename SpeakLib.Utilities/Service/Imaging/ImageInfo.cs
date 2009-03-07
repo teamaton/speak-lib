fedff74e1909dea966b1a7ce603da1c60494e5e2
noackstefan <noackstefan@63a8470e-8155-0410-a357-b11873b86f21>
2009-03-07 01:44:57 +0000

    image store: groups
    git-svn-id: https://speak-lib.googlecode.com/svn/trunk@88 63a8470e-8155-0410-a357-b11873b86f21

---
+++ SpeakLib.Utilities/Service/Imaging/ImageInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpeakFriend.Utilities
+{
+    public class ImageInfo
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public string AbsolutePath { get; set; }
+        public string RelativePath { get; set; }
+    }
+}
--- SpeakLib.Utilities/Service/Imaging/ImageStore.cs
+++ SpeakLib.Utilities/Service/Imaging/ImageStore.cs
@@ -19,26 +19,90 @@ namespace SpeakFriend.Utilities
             _pathRelative = pathRelative;
         }
 
-        public void Store(string key, string sourcePath)
+        public void Store(string imageKey, string sourcePath)
         {
+
             var image = Image.FromFile(sourcePath);
-            image.Save(GetPathAbsolute(key), ImageFormat.Png);
+            image.Save(GetPathAbsolute(imageKey), ImageFormat.Png);
+        }
+
+        public void StoreToGroup(string groupKey, string sourcePath, string name)
+        {
+            var groupDirectory = GetGroupDirectoryAbsolute(groupKey);
+            Directory.CreateDirectory(groupDirectory);
+
+            var images = GetGroup(groupKey);
+            var id = images.Count == 0 ? 0 : images.Max(img => img.Id) + 1;
+            var path = GetPathAbsolute(groupKey, string.Format("{0}-{1}.png", id, name));
+
+            var image = Image.FromFile(sourcePath);
+            image.Save(path, ImageFormat.Png);
         }
   
-        public string Get(string key)
+        public ImageInfo Get(string imageKey)
         {
             //ToDo: check if exists and if not, return default placeholder
-            return GetPathRelative(key);
+            return new ImageInfo
+                       {
+                           AbsolutePath = GetPathAbsolute(imageKey),
+                           RelativePath = GetPathRelative(imageKey),
+                           Name = imageKey,
+                           Id = -1
+                       };
         }
 
-        private string GetPathAbsolute(string key)
+
+        public ImageInfo Get(string groupKey, int id)
         {
-            return Path.Combine(_pathAbsolute, string.Format("{0}.png", key));
+            return GetGroup(groupKey).Find(image => image.Id == id);
         }
 
-        private string GetPathRelative(string key)
+        public List<ImageInfo> GetGroup(string groupKey)
         {
-            return Path.Combine(_pathRelative, string.Format("{0}.png", key));
+            if(!Directory.Exists(GetGroupDirectoryAbsolute(groupKey))) 
+                return new List<ImageInfo>();
+
+            return
+                Directory.GetFiles(GetGroupDirectoryAbsolute(groupKey)).ToList().ConvertAll(
+                    path =>
+                        {
+                            var filename = Path.GetFileNameWithoutExtension(path);
+                            var extension = Path.GetExtension(path);
+                            return new ImageInfo()
+                                       {
+                                           Id = Convert.ToInt32(filename.Split('-').First()),
+                                           AbsolutePath = path,
+                                           RelativePath = GetPathRelative(groupKey, filename + extension),
+                                           Name = filename.Substring(filename.IndexOf('-') + 1)
+                                       };
+                        });
+            
         }
+
+        private string GetPathRelative(string groupKey, string filename)
+        {
+            return Path.Combine(Path.Combine(_pathRelative, groupKey), filename);
+        }
+
+        private string GetPathAbsolute(string groupKey, string filename)
+        {
+            return Path.Combine(Path.Combine(_pathAbsolute, groupKey), filename);
+        }
+
+        private string GetGroupDirectoryAbsolute(string groupKey)
+        {
+            return Path.Combine(_pathAbsolute, groupKey);
+        }
+
+        private string GetPathAbsolute(string imageKey)
+        {
+            return Path.Combine(_pathAbsolute, string.Format("{0}.png", imageKey));
+        }
+
+        private string GetPathRelative(string imageKey)
+        {
+            return Path.Combine(_pathRelative, string.Format("{0}.png", imageKey));
+        }
+
     }
 }
