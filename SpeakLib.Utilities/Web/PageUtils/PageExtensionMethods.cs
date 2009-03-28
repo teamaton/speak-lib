a9f835f3fb4a3498c76706dc73a41cf823a1a65a
oliver.friedrich <oliver.friedrich@63a8470e-8155-0410-a357-b11873b86f21>
2009-03-28 14:09:19 +0000

    PageExtensionMethods: AddStyleSheet
    git-svn-id: https://speak-lib.googlecode.com/svn/trunk@131 63a8470e-8155-0410-a357-b11873b86f21

---
+++ SpeakLib.Utilities/Web/PageUtils/PageExtensionMethods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+
+namespace SpeakFriend.Utilities
+{
+    public static class PageExtensionMethods
+    {
+        /// <summary>
+        /// Add a CSS file reference to the HEAD section of a Page.
+        /// </summary>
+        /// <param name="page">The Page instance to which to add the CSS file reference.</param>
+        /// <param name="cssFileName">The application relative path of the CSS file to add.</param>
+        public static void AddStyleSheet(this Page page, string cssFileName)
+        {
+            var htmlLink = new HtmlLink();
+            htmlLink.Href = cssFileName;
+            htmlLink.Attributes.Add("rel", "Stylesheet");
+            htmlLink.Attributes.Add("type", "text/css");
+            htmlLink.Attributes.Add("media", "all");
+            page.Header.Controls.Add(htmlLink);
+        }
+    }
+}
