45a41b16757b2077f6b8ae5e1e67c65a0ca61e99
RobertMischke <RobertMischke@63a8470e-8155-0410-a357-b11873b86f21>
2009-03-31 21:16:11 +0000

    Introduced ISessionManager, SessionFactoryContainer, etc.
    git-svn-id: https://speak-lib.googlecode.com/svn/trunk@140 63a8470e-8155-0410-a357-b11873b86f21

--- ExampleApp.Core/Infrastructure/Autofac_CoreModule.cs
+++ ExampleApp.Core/Infrastructure/Autofac_CoreModule.cs
@@ -2,10 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Autofac.Builder;
+using SpeakFriend.Utilities;
 
 namespace SpeakFriend.ExampleApp.Core
 {
-    class Autofac_CoreModule
+    public class Autofac_CoreModule : Module
     {
+        protected override void Load(ContainerBuilder builder)
+        {
+            Setting(builder);
+        }
+
+        private void Setting(ContainerBuilder builder)
+        {
+            builder
+                .Register(c => new SettingRepository(c.Resolve<SessionManager>().Session))
+                .As<ISettingRepository>()
+                .ContainerScoped();
+
+            builder
+                .Register(c => new SettingService(c.Resolve<ISettingRepository>()))
+                .As<SettingService>()
+                .ContainerScoped();
+        }
+
     }
 }
---
+++ SpeakLib.Utilities/Persistance.NHibernate/SessionFactoryContainer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NHibernate;
+using NHibernate.Cfg;
+
+namespace SpeakFriend.Utilities
+{
+    public class SessionFactoryContainer
+    {
+        private ISessionFactory _sessionFactory;
+        public ISessionFactory GetSessionFactory()
+        {
+            if (_sessionFactory == null)
+                _sessionFactory = new Configuration().Configure().BuildSessionFactory();
+
+            return _sessionFactory;
+        }
+    }
+}
