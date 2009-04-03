989637e1bb8f1fd9f6cf8fda3859cd8304d37ca0
noackstefan <noackstefan@63a8470e-8155-0410-a357-b11873b86f21>
2009-04-03 15:55:30 +0000

    hover controls
    git-svn-id: https://speak-lib.googlecode.com/svn/trunk@143 63a8470e-8155-0410-a357-b11873b86f21

---
+++ SpeakLib.Utilities/Web/Controls/HoverImage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+
+namespace SpeakFriend.Utilities
+{
+    [ToolboxData("<{0}:HoverImage runat=server></{0}:HoverImage>")]
+    public class HoverImage : HtmlImage
+    {
+        public HoverImage()
+        {
+            HoverSuffix = "-on";
+        }
+
+        [Bindable(false), Category("Appearance"), DefaultValue("-on"), Localizable(false)]
+        public string HoverSuffix { get; set; }
+
+        protected override void Render(HtmlTextWriter writer)
+        {
+            var hoverSrc = Src.Insert(Src.IndexOf('.'), HoverSuffix);
+
+            Attributes.Add("onmouseover", string.Format("src='{0}'", hoverSrc));
+            Attributes.Add("onmouseout", string.Format("src='{0}'", Src));
+            
+            base.Render(writer);
+        }
+    }
+}
