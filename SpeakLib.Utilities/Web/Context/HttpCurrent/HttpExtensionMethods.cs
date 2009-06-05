fcfb6de2af154e4daa6d3f792c8a047dd3b86d9b
oliver.friedrich <oliver.friedrich@63a8470e-8155-0410-a357-b11873b86f21>
2009-06-05 09:48:15 +0000

    RedirectToSamePage als ExtensionMethod :-)
    git-svn-id: https://speak-lib.googlecode.com/svn/trunk@181 63a8470e-8155-0410-a357-b11873b86f21

---
+++ SpeakLib.Utilities/Web/Context/HttpCurrent/HttpExtensionMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace SpeakFriend.Utilities.Web
+{
+	public static class ResponseHelper
+	{
+		public static void RedirectToSamePage(this HttpResponse response, HttpRequest request)
+		{
+			response.Redirect(request.RawUrl);
+		}
+	}
+}
