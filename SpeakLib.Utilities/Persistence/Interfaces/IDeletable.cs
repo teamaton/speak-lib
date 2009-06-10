92ef2791d9502978c8f3c82018550fec079bc38f
oliver.friedrich <oliver.friedrich@63a8470e-8155-0410-a357-b11873b86f21>
2009-06-10 19:40:38 +0000

    added IDeletable
    git-svn-id: https://speak-lib.googlecode.com/svn/trunk@188 63a8470e-8155-0410-a357-b11873b86f21

---
+++ SpeakLib.Utilities/Persistence/Interfaces/IDeletable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpeakFriend.Utilities
+{
+    public interface IDeletable 
+    {
+		bool Deleted { get; set; }
+    }
+}
