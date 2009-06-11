e81c9f29f32c524494f3a1e2b2e95998a2824bb9
anton.telle <anton.telle@63a8470e-8155-0410-a357-b11873b86f21>
2009-06-11 17:06:23 +0000

    einige Enums für den User
    git-svn-id: https://speak-lib.googlecode.com/svn/trunk@190 63a8470e-8155-0410-a357-b11873b86f21

---
+++ SpeakLib.Utilities/ValueObjects/Gender.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpeakFriend.Utilities
+{
+    public enum Gender
+    {
+        Unspecified,
+        Male,
+        Female
+    }
+}
---
+++ SpeakLib.Utilities/ValueObjects/ProfileVisibility.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpeakFriend.Utilities
+{
+    public enum ProfileVisibility
+    {
+        None,
+        All,
+        Friends,
+        Users
+    }
+}
