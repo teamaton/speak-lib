8796517e1ce5126b61dd4032795c1ef001974317
RobertMischke <RobertMischke@63a8470e-8155-0410-a357-b11873b86f21>
2009-06-13 23:29:58 +0000

    Added new BaseSearchDesc, removes some repetitive boiler plate code
    git-svn-id: https://speak-lib.googlecode.com/svn/trunk@194 63a8470e-8155-0410-a357-b11873b86f21

---
+++ SpeakLib.Utilities/Persistence.Query/SearchDescriptionBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpeakFriend.Utilities
+{
+    public class SearchDescriptionBase<TFilter, TOrderBy> : Pager, ISearchDesc
+        where TFilter : ConditionContainer, new()
+        where TOrderBy : OrderByCriteria, new()
+    {
+        ConditionContainer ISearchDesc.Filter { get { return Filter; } }
+        OrderByCriteria ISearchDesc.OrderBy { get { return OrderBy; } }
+
+        private readonly TFilter _filter = new TFilter();
+        private readonly TOrderBy _orderBy = new TOrderBy();
+
+        public TFilter Filter { get { return _filter; } }
+        public TOrderBy OrderBy { get { return _orderBy; } }
+    }
+}
