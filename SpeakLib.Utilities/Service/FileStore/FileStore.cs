b5889967d65b824c350727ab12dd635dc9c764cb
noackstefan <noackstefan@63a8470e-8155-0410-a357-b11873b86f21>
2009-08-18 06:17:31 +0000

    minor improvements to FileStore
    git-svn-id: https://speak-lib.googlecode.com/svn/trunk@228 63a8470e-8155-0410-a357-b11873b86f21

--- SpeakLib.Utilities/Service/FileStore/FileStore.cs
+++ SpeakLib.Utilities/Service/FileStore/FileStore.cs
@@ -12,11 +12,13 @@ namespace SpeakFriend.Utilities
     {
         private readonly string _pathAbsolute;
         private readonly string _pathRelative;
+        private readonly Func<Type, string> _typeNames;
 
-        public FileStore(ISession session, string pathAbsolute, string pathRelative) : base(session)
+        public FileStore(ISession session, string pathAbsolute, string pathRelative, Func<Type,String> typeNames) : base(session)
         {
             _pathAbsolute = pathAbsolute;
             _pathRelative = pathRelative;
+            _typeNames = typeNames;
         }
 
         public StoredFile Store(string sourcePath, IPersistable entity)
@@ -29,13 +31,13 @@ namespace SpeakFriend.Utilities
             var storedFile = new StoredFile
             {
                 OriginalFileName = Path.GetFileName(fileName),
-                EntityType = "FIXME!",
+                EntityType = _typeNames(entity.GetType()),
                 EntityId = entity.Id
             };
 
             Create(storedFile);
 
-            File.Copy(sourcePath, GetPathAbsolute(storedFile));
+            File.Copy(sourcePath, GetPathAbsolute(storedFile), true);
 
             return storedFile;
         }
@@ -49,6 +51,11 @@ namespace SpeakFriend.Utilities
         {
             return string.Format("File{0}.dat", storedFile.Id);
         }
-       
+
+        public void DeleteDataFiles()
+        {
+            foreach (var file in Directory.GetFiles(_pathAbsolute))
+                File.Delete(file);
+        }
     }
 }
