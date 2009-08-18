334f83bfc67964e2e978b4209be1a5ab24321fb4
oliver.friedrich <oliver.friedrich@63a8470e-8155-0410-a357-b11873b86f21>
2009-08-18 18:43:31 +0000

    Shuffle() extension method for IList (using Fisher-Yates algo)
    git-svn-id: https://speak-lib.googlecode.com/svn/trunk@229 63a8470e-8155-0410-a357-b11873b86f21

---
+++ SpeakLib.Utilities/Service/ExtensionMethods/CollectionExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpeakFriend.Utilities
+{
+	public static class CollectionExtensions
+	{
+		/// <summary>
+		/// Shuffles the items in the list, changes the list!
+		/// </summary>
+		/// <remarks>
+		/// User Fisher-Yates (see http://en.wikipedia.org/wiki/Fisher-Yates_shuffle)
+		/// recommended here: http://nunos.zi-yu.com/2008/03/shuffle-a-collection/
+		/// </remarks>
+		public static void Shuffle(this IList elements)
+		{
+			var random = new Random();
+			int n = elements.Count;
+			while (n > 1)
+			{
+				int k = random.Next(n);
+				--n;
+				var temp = elements[n];
+				elements[n] = elements[k];
+				elements[k] = temp;
+			}
+		}
+	}
+}
