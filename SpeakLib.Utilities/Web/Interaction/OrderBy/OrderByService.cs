46a08d5c2271a61bf3cc611f576d6cb7cf8b65e8
noackstefan <noackstefan@63a8470e-8155-0410-a357-b11873b86f21>
2009-08-25 22:51:03 +0000

    OrderByService
    git-svn-id: https://speak-lib.googlecode.com/svn/trunk@236 63a8470e-8155-0410-a357-b11873b86f21

---
+++ SpeakLib.Utilities/Web/Interaction/OrderBy/OrderByService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.UI.WebControls;
+
+namespace SpeakFriend.Utilities.Web
+{
+    public class OrderByService
+    {
+        public OrderByService RegisterButton(LinkButton button, OrderBy orderBy)
+        {
+            button.Click += delegate { ButtonClicked(orderBy); };
+            SetCssClass(button, orderBy);
+            Changed += delegate { SetCssClass(button, orderBy); };
+            return this;
+        }
+ 
+        private void ButtonClicked(OrderBy orderBy)
+        {
+            orderBy.AscOrToggle();
+            OnChanged(EventArgs.Empty);
+        }
+
+        private void SetCssClass(LinkButton button, OrderBy orderBy)
+        {
+            button.CssClass = "";
+            if (orderBy.IsCurrent()) button.AddCssClass("current");
+            button.AddCssClass(orderBy.IsAsc() ? "sort-up" : "sort-down");
+        }
+
+        private void OnChanged(EventArgs e)
+        {
+            if (Changed != null) Changed(this, e);
+        }
+
+        public event EventHandler Changed;
+    }
+}
