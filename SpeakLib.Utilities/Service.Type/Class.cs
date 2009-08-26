c8975444308c9c868a74dc6afcc610f66067b5cf
RobertMischke <RobertMischke@63a8470e-8155-0410-a357-b11873b86f21>
2009-08-26 15:31:57 +0000

    Added Class.DeepClone
    git-svn-id: https://speak-lib.googlecode.com/svn/trunk@237 63a8470e-8155-0410-a357-b11873b86f21

---
+++ SpeakLib.Utilities/Service.Type/Class.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+namespace SpeakFriend.Utilities
+{
+    public class Class
+    {
+        /// <summary>
+        /// Clones all serializiable members of class, 
+        /// the class must be serializable ([Serializable]).
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        /// <remarks>
+        /// http://stackoverflow.com/questions/129389/how-do-you-do-a-deep-copy-an-object-in-net-c-specifically
+        /// </remarks>
+        public static T DeepClone<T>(T obj)
+        {
+            using (var ms = new MemoryStream())
+            {
+                var formatter = new BinaryFormatter();
+                formatter.Serialize(ms, obj);
+                ms.Position = 0;
+
+                return (T)formatter.Deserialize(ms);
+            }
+        }
+    }
+}
