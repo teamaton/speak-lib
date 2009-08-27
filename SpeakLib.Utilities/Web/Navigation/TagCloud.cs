641c52cbf0fd43c0721bbdcad5fdd46f984586f8
oliver.friedrich <oliver.friedrich@63a8470e-8155-0410-a357-b11873b86f21>
2009-08-27 18:45:39 +0000

    new class TagCloud wraps logic to assign CSS to certain values within a tag cloud;
    git-svn-id: https://speak-lib.googlecode.com/svn/trunk@239 63a8470e-8155-0410-a357-b11873b86f21

---
+++ SpeakLib.Utilities/Web/Navigation/TagCloud.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpeakFriend.Utilities.Web
+{
+	public class TagCloud
+	{
+		public int MinValue { get; private set; }
+		public int MaxValue { get; private set; }
+
+		public List<string> CssClasses = new List<string>();
+		public int CssClassCount { get { return CssClasses.Count; } }
+
+		public TagCloud(int minValue, int maxValue, params string[] cssClasses)
+		{
+			if (MaxValue < MinValue)
+				throw new ArgumentException("maxValue must be larger than minValue!", "maxValue");
+			if (cssClasses == null || cssClasses.Count() < 1)
+				throw new ArgumentException("cssClasses must not be null and must contain at least" +
+				                            " one value!", "cssClasses");
+
+			MinValue = minValue;
+			MaxValue = maxValue;
+			CssClasses.AddRange(cssClasses);
+		}
+
+		public string GetCssClass(int value)
+		{
+			decimal step = (MaxValue - MinValue) / (decimal) CssClassCount;
+			decimal lower = MinValue;
+			int slot = 0;
+			while (lower + step < value)
+			{
+				lower += step;
+				slot++;
+			}
+			return CssClasses[slot];
+		}
+	}
+}
