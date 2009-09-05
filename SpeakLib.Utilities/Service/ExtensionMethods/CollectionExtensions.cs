aad7060425e48d3df3cd5fcb8995f0865bd13517
oliver.friedrich <oliver.friedrich@63a8470e-8155-0410-a357-b11873b86f21>
2009-09-05 14:54:36 +0000

    added strongly typed version of Shuffle() which returns the given list
    git-svn-id: https://speak-lib.googlecode.com/svn/trunk@245 63a8470e-8155-0410-a357-b11873b86f21

--- SpeakLib.Utilities/Service/ExtensionMethods/CollectionExtensions.cs
+++ SpeakLib.Utilities/Service/ExtensionMethods/CollectionExtensions.cs
@@ -15,7 +15,7 @@ namespace SpeakFriend.Utilities
 		/// User Fisher-Yates (see http://en.wikipedia.org/wiki/Fisher-Yates_shuffle)
 		/// recommended here: http://nunos.zi-yu.com/2008/03/shuffle-a-collection/
 		/// </remarks>
-		public static void Shuffle(this IList elements)
+		public static IList Shuffle(this IList elements)
 		{
 			var random = new Random(DateTime.Now.Millisecond);
 			int n = elements.Count;
@@ -27,6 +27,30 @@ namespace SpeakFriend.Utilities
 				elements[n] = elements[k];
 				elements[k] = temp;
 			}
+
+			return elements;
+		}
+
+		/// <summary>
+		/// Strongly typed version of Shuffle().
+		/// </summary>
+		/// <typeparam name="TList"></typeparam>
+		/// <param name="list"></param>
+		/// <returns></returns>
+		public static TList Shuffle<TList>(this TList list) where TList : IList
+		{
+			var random = new Random(DateTime.Now.Millisecond);
+			int n = list.Count;
+			while (n > 1)
+			{
+				int k = random.Next(n);
+				--n;
+				var temp = list[n];
+				list[n] = list[k];
+				list[k] = temp;
+			}
+
+			return list;
 		}
 	}
 }
