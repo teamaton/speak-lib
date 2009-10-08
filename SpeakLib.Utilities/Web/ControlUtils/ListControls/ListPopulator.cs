ec45334a33b4f96facafa131b398c335c47cd345
oliver.friedrich <oliver.friedrich@63a8470e-8155-0410-a357-b11873b86f21>
2009-10-08 16:09:42 +0000

    list populator with weekdays;
    git-svn-id: https://speak-lib.googlecode.com/svn/trunk@269 63a8470e-8155-0410-a357-b11873b86f21

--- SpeakLib.Utilities/Web/ControlUtils/ListControls/ListPopulator.cs
+++ SpeakLib.Utilities/Web/ControlUtils/ListControls/ListPopulator.cs
@@ -27,5 +27,38 @@ namespace SpeakFriend.Utilities
                 date = date.AddMonths(1);
             }
         }
+
+        /// <summary>
+        /// Adds all months of the year as <see cref="ListItem"/>s to the given ListControl.
+        /// </summary>
+        /// <param name="listControl"></param>
+        /// <param name="cultureInfo"></param>
+        public static void PopulateWeekDayNames(this ListControl listControl, CultureInfo cultureInfo)
+        {
+			// Use this approach rather than DateTimeFormatInfo.GetInstance(cultureInfo).DayNames
+			// because the first day of the week differs between cultures!!!
+
+			var date = new DateTime(2009, 10, 5); // used only for the weekday: 05.10.2009 was a Monday
+
+			// Monday is our first day of the week.
+			for (int i = 1; i <= 7; i++)
+			{
+				listControl.Items.Add(new ListItem(
+					date.ToString("dddd", cultureInfo.NumberFormat), // full week day as text
+					i.ToString())); 
+
+				date = date.AddDays(1);
+			}
+        }
+
+    	/// <summary>
+		/// Adds the given numbers as <see cref="ListItem"/>s to the given ListControl.
+		/// </summary>
+		public static void PopulateNumbers(this ListControl listControl, int min, int max, int step)
+		{
+			listControl.Items.Clear();
+			for (int i = min; i <= max; i += step)
+				listControl.Items.Add(new ListItem(i.ToString(), i.ToString()));
+		}
     }
 }
