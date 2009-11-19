a9ff2c0082428771b2c093663dc02a792b19dfb5
noackstefan <noackstefan@63a8470e-8155-0410-a357-b11873b86f21>
2009-11-19 13:29:39 +0000

    PersistableList NH mapping
    git-svn-id: https://speak-lib.googlecode.com/svn/trunk@284 63a8470e-8155-0410-a357-b11873b86f21

--- SpeakLib.Utilities/Persistence/PersistableList.cs
+++ SpeakLib.Utilities/Persistence/PersistableList.cs
@@ -2,12 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Iesi.Collections.Generic;
 
 namespace SpeakFriend.Utilities
 {
     [Serializable]
     public abstract class PersistableList<T> : List<T> where T : IPersistable
     {
+        private ISet<T> _persistentItems = new HashedSet<T>();
+        private ISet<T> Items
+        {
+            get
+            {
+                _persistentItems.Clear();
+                _persistentItems.AddAll(this);
+                return _persistentItems;
+            }
+            set
+            {
+                _persistentItems = value;
+                Clear();
+                AddRange(value);
+            }
+        }
+
         protected PersistableList(){}
 
         protected PersistableList(IEnumerable<T> list)
