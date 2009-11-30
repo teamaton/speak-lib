301f0d32a032b6de7c8d1120dbdc2eb1172654b3
oliver.friedrich <oliver.friedrich@63a8470e-8155-0410-a357-b11873b86f21>
2009-11-30 09:36:51 +0000

    included GetIpAddress() extension method for Request, renamed ResponseHelper -> RequestResponseExtensions
    git-svn-id: https://speak-lib.googlecode.com/svn/trunk@299 63a8470e-8155-0410-a357-b11873b86f21

--- SpeakLib.Utilities/Web/Context/HttpCurrent/HttpExtensionMethods.cs
+++ SpeakLib.Utilities/Web/Context/HttpCurrent/HttpExtensionMethods.cs
@@ -6,11 +6,30 @@ using System.Web;
 
 namespace SpeakFriend.Utilities.Web
 {
-	public static class ResponseHelper
+	public static class RequestResponseExtensions
 	{
 		public static void RedirectToSamePage(this HttpResponse response, HttpRequest request)
 		{
 			response.Redirect(request.RawUrl);
+		}
+
+		public static string GetIpAddress(this HttpRequest request)
+		{
+			var ip = request.UserHostAddress;
+
+			if (!string.IsNullOrEmpty(ip))
+				return ip;
+
+			// See: http://forums.asp.net/p/1053767/1496008.aspx
+
+			ip = request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+
+			// If there is no proxy, get the standard remote address
+
+			if (string.IsNullOrEmpty(ip) || (ip.ToLowerInvariant() == "unknown"))
+				ip = request.ServerVariables["REMOTE_ADDR"];
+
+			return ip ?? "unknown";
 		}
 	}
 }
