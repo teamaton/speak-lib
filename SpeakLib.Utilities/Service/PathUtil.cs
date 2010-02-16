6e5e2103b60f0849e4a39227ba807e620c6996b5
Stefan Noack <stefan@speak-friend.com>
2010-02-16 09:15:49 +0100

    PathUtil

---
+++ SpeakLib.Utilities/Service/PathUtil.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace SpeakFriend.Utilities
+{
+    public static class PathUtil
+    {
+        public static string GetRelativePath(string path, string rootDirectory)
+        {
+            return path.Replace(rootDirectory + Path.DirectorySeparatorChar, "");
+        }
+    }
+}
