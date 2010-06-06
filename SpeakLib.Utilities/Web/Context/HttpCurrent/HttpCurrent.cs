740714a5b8f71131be484d5b7b9526baa73c3c9b
Oliver <oliver.friedrich@gmail.com>
2010-06-06 09:33:10 +0200

    Addition of Context to HttpCurrent

--- SpeakLib.Utilities/Web/Context/HttpCurrent/HttpCurrent.cs
+++ SpeakLib.Utilities/Web/Context/HttpCurrent/HttpCurrent.cs
@@ -9,32 +9,38 @@ namespace SpeakFriend.Utilities.Web
 {
     public interface IHttpCurrent
     {
-        IRequest Request { get;  }
-        IResponse Response { get;  }
+        IRequest Request { get; }
+        IResponse Response { get; }
+        HttpContext Context { get; }
     }
 
     public class HttpCurrent : IHttpCurrent
     {
         public virtual IRequest Request { get; protected set; }
         public virtual IResponse Response { get; protected set; }
-
+        public virtual HttpContext Context { get; protected set; }
 
         protected HttpCurrent(){}
 
         public static HttpCurrent Get()
         {
-            var result = new HttpCurrent();
-            result.Request = new RequestWeb();
-            result.Response = new ResponseWeb();
+            var result = new HttpCurrent
+                             {
+                                 Request = new RequestWeb(),
+                                 Response = new ResponseWeb(),
+                                 Context = HttpContext.Current
+                             };
 
             return result;
         }
 
         public static HttpCurrentNoWeb GetNoWeb()
         {
-            var result = new HttpCurrentNoWeb();
-            result.Request = new RequestNoWeb();
-            result.Response = new ResponseNoWeb();
+            var result = new HttpCurrentNoWeb
+                             {
+                                 Request = new RequestNoWeb(),
+                                 Response = new ResponseNoWeb()
+                             };
 
             return result;
         }
--- SpeakLib.Utilities/Web/Context/HttpCurrent/HttpCurrentNoWeb.cs
+++ SpeakLib.Utilities/Web/Context/HttpCurrent/HttpCurrentNoWeb.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
+using System.Web;
 
 namespace SpeakFriend.Utilities.Web
 {
@@ -10,9 +11,11 @@ namespace SpeakFriend.Utilities.Web
     {
         IResponse IHttpCurrent.Response { get { return Response; } }
         IRequest IHttpCurrent.Request { get { return Request; } }
+        HttpContext IHttpCurrent.Context { get { throw new NotImplementedException("HttpContext not yet implemented for NoWeb!"); } }
 
         public RequestNoWeb Request { get; set; }
         public ResponseNoWeb Response { get; set; }
+        public HttpContext Context { get; set; }
 
         public void Reset()
         {
