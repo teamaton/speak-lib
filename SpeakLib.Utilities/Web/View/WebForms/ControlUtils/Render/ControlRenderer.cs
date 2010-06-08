872392a82f0d0bb05698490d19da348438d30bd8
Oliver <oliver.friedrich@gmail.com>
2010-06-09 01:38:04 +0200

    Moved some UserControl rendering stuff (e.g. for WCF services) here

---
+++ SpeakLib.Utilities/Web/View/WebForms/ControlUtils/Render/ControlRenderer.cs
@@ -0,0 +1,64 @@
+using System;
+using System.IO;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.UI;
+
+namespace SpeakFriend.Utilities.Web.Render
+{
+    public static class ControlRenderer
+    {
+        /// <summary>
+        /// Renders the given UserControls in the given order onto an empty Page object and returns the rendered string.
+        /// </summary>
+        public static string RenderControl (params string[] pathToUserControl)
+        {
+            var pageHolder = new Page();
+            foreach (var path in pathToUserControl)
+            {
+                var viewControl = (UserControl)pageHolder.LoadControl(path);
+                pageHolder.Controls.Add(viewControl);
+            }
+            var output = new StringWriter();
+            HttpContext.Current.Server.Execute(pageHolder, output, true);
+
+            return output.ToString();
+        }
+
+        public static string RenderControl<T>(string pathToControl, Action<T> modifyControl)
+            where T : UserControl
+        {
+            var page = new Page();
+            var ctl = (T)page.LoadControl(pathToControl);
+
+            if (modifyControl != null)
+                modifyControl(ctl);
+
+            page.Controls.Add(ctl);
+            var writer = new StringWriter();
+            HttpContext.Current.Server.Execute(page, writer, true);
+            return writer.ToString();
+        }
+
+        public static string RemoveHtmlSpace(this string html)
+        {
+            return Regex.Replace(Regex.Replace(html.Trim(), @"\>\s+\<", "><", RegexOptions.Singleline)
+                                 , @"\s+\</", "</", RegexOptions.Singleline);
+        }
+
+        /// <summary>
+        /// Returns a UTF-8 encoded MemoryStream from the given string that can be used as a response to an HTTP request.
+        /// </summary>
+        public static Stream ToResponseStream(this string output)
+        {
+            if (WebOperationContext.Current != null)
+                WebOperationContext.Current.OutgoingResponse.ContentType = "text/plain";
+
+            //trick to output exactly what you want (without wcf wrapping it)
+            return new MemoryStream(Encoding.UTF8.GetBytes(output));
+        }
+
+    }
+}
