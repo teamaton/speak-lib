ea1df70e12d38b1c5381a20d1826ee767c87d013
Oliver <oliver.friedrich@gmail.com>
2010-09-13 10:31:31 +0200

    DateTimeExtensions with ToMilliseconds

---
+++ SpeakLib.Utilities/Service/ExtensionMethods/DateTimeExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpeakFriend.Utilities
+{
+	public static class DateTimeExtensions
+	{
+		private static readonly DateTime DateTime01011970 = new DateTime(1970, 1, 1);
+
+		/// <summary>
+		/// Returns the number of milliseconds that elapsed from 01/01/1970 to the given <paramref name="dateTime"/>.
+		/// </summary>
+		public static long ToMilliseconds(this DateTime dateTime)
+		{
+			return Convert.ToInt64((dateTime - DateTime01011970).TotalMilliseconds);
+		}
+	}
+}
