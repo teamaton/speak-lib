5d4e8c78b0a1b85b7707af8207af5b94d4706723
Oliver <oliver.friedrich@gmail.com>
2010-09-18 03:29:08 +0200

    created PersistableBase as base implementation of IPersistable

--- SpeakLib.Utilities/Persistence/Interfaces/IPersistable.cs
+++ SpeakLib.Utilities/Persistence/Interfaces/IPersistable.cs
@@ -1,14 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace SpeakFriend.Utilities
 {
-    public interface IPersistable
-    {
-        int Id { get; set; }
+	public interface IPersistable
+	{
+		int Id { get; set; }
 
-        DateTime DateCreated { get; set; }
-    }
+		DateTime DateCreated { get; set; }
+	}
 }
---
+++ SpeakLib.Utilities/Persistence/Interfaces/PersistableBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SpeakFriend.Utilities
+{
+	/// <summary>
+	/// Implements <see cref="IPersistable"/> with <see cref="Id"/> and <see cref="DateCreated"/>, 
+	/// and initializes <see cref="DateCreated"/> with the current <see cref="DateTime"/> in the constructor.
+	/// </summary>
+	public abstract class PersistableBase : IPersistable
+	{
+		protected PersistableBase()
+		{
+			_dateCreated = DateTime.Now;
+		}
+
+		#region Implementation of IPersistable
+
+		public virtual int Id { get; set; }
+		private DateTime _dateCreated;
+
+		public virtual DateTime DateCreated
+		{
+			get { return _dateCreated; }
+			set { _dateCreated = value; }
+		}
+
+		#endregion
+	}
+}
