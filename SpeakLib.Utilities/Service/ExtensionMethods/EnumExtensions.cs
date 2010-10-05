fe49ce059fc957614936e18c06bbf98ae4dcc2b4
Oliver <oliver.friedrich@gmail.com>
2010-10-05 14:49:28 +0200

    moved EnumExtension here

---
+++ SpeakLib.Utilities/Service/ExtensionMethods/EnumExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SpeakFriend.Utilities
+{
+    public static class EnumExtensions
+    {
+        public static int ToInt(this Enum en)
+        {
+            return Convert.ToInt32(en);
+        }
+    }
+}
