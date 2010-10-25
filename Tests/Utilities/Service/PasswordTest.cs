24dc21b78f2782995c39a72c3ff81c9fc4344fcf
Oliver <oliver.friedrich@gmail.com>
2010-10-25 16:12:22 +0200

    new Password class with salt and salted hash incl. Test

---
+++ Tests/Utilities/Service/PasswordTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+using SpeakFriend.Utilities;
+
+namespace Tests.Utilities.Web
+{
+	[TestFixture]
+	public class PasswordTest
+	{
+		[Test]
+		public void Usage()
+		{
+			var passwordPlainText = "Passwort";
+
+			// Create
+			var pw = new Password(passwordPlainText);
+
+			// Save
+			Console.WriteLine("Salt:        {0}", pw.Salt);
+			Console.WriteLine("Salted hash: {0}", pw.SaltedHash);
+
+			pw.Matches(passwordPlainText).Should().Be.True();
+		}
+	}
+}
