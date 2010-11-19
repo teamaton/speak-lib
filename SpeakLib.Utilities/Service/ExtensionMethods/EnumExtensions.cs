667b9c660cd65fd7346bb2f3b883f85c9e37dc99
Oliver <oliver.friedrich@gmail.com>
2010-11-19 20:03:37 +0100

    Enum methods and ListControl population (moved to ListPopulator)

--- SpeakLib.Utilities/Service/ExtensionMethods/EnumExtensions.cs
+++ SpeakLib.Utilities/Service/ExtensionMethods/EnumExtensions.cs
@@ -8,5 +8,10 @@ namespace SpeakFriend.Utilities
         {
             return Convert.ToInt32(en);
         }
+
+        public static string ToIntString(this Enum en)
+        {
+            return Convert.ToInt32(en).ToString();
+        }
     }
 }
