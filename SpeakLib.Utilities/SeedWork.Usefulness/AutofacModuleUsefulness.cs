42c0c1a4ec7ef31d9fa23db4af317687ffd6ab9a
Oliver <oliver.friedrich@gmail.com>
2010-12-07 23:07:14 +0100

    cleaned up Project references: removed 4 libs

--- SpeakLib.Utilities/SeedWork.Usefulness/AutofacModuleUsefulness.cs
+++ SpeakLib.Utilities/SeedWork.Usefulness/AutofacModuleUsefulness.cs
@@ -1,28 +1,15 @@
-﻿using System;
-using Autofac;
-using Autofac.Builder;
-using AutofacContrib.NHibernate;
-using NHibernate.ByteCode.LinFu;
-using NHibernate.Type;
-using Environment=NHibernate.Cfg.Environment;
+﻿using Autofac.Builder;
+using SpeakFriend.Utilities;
 
 namespace SpeakFriend.Utilities.Usefulness
 {
 	public class AutofacModuleUsefulness : Module
 	{
-		public override void Configure(IContainer container)
-		{
-			base.Configure(container);
-
-			Environment.BytecodeProvider = new AutofacBytecodeProvider(
-				container, new ProxyFactoryFactory(), new DefaultCollectionTypeFactory());
-		}
-
 		protected override void Load(ContainerBuilder builder)
 		{
 			builder.Register<UsefulnessService>().ContainerScoped();
 
-			builder.RegisterTypesMatching(t => typeof(IUsefulnessEntity).IsAssignableFrom(t)).FactoryScoped();
+			builder.RegisterTypesMatching(t => typeof (IUsefulnessEntity).IsAssignableFrom(t)).FactoryScoped();
 		}
 	}
 }
--- SpeakLib.Utilities/Web/Context/Cache/CacheAnalyzer/CacheItemTypeSummaryList.cs
+++ SpeakLib.Utilities/Web/Context/Cache/CacheAnalyzer/CacheItemTypeSummaryList.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using NUnit.Framework;
 using SpeakFriend.Utilities.ValueObjects;
 
 namespace SpeakFriend.Utilities.Web.Analysis
