078e04155c4b3bf9a409a48c5d3d307fe0863a16
Oliver <oliver.friedrich@gmail.com>
2011-01-20 03:11:31 +0100

    base class mappings for Persistable+MutablePersistable

---
+++ SpeakLib.Utilities/Persistence/Interfaces/Mappings.cs
@@ -0,0 +1,23 @@
+﻿using FluentNHibernate.Mapping;
+
+namespace SpeakFriend.Utilities
+{
+	public abstract class PersistableBaseMapping<T> : ClassMap<T>
+		where T : PersistableBase
+	{
+		protected PersistableBaseMapping()
+		{
+			Id(x => x.Id);
+			Map(x => x.DateCreated);
+		}
+	}
+
+	public abstract class MutablePersistableBaseMapping<T> : PersistableBaseMapping<T>
+		where T : MutablePersistableBase
+	{
+		protected MutablePersistableBaseMapping()
+		{
+			Map(x => x.DateModified);
+		}
+	}
+}
