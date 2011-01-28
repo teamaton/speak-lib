31389967dea5bfecbe070801f41949610878d5ee
Oliver <oliver.friedrich@gmail.com>
2011-01-28 22:29:05 +0100

    added c'tor to BaseSetup, made methods virtual; added comments to fluent mappings

--- SpeakLib.Utilities/Persistence.Setup/BaseSetup.cs
+++ SpeakLib.Utilities/Persistence.Setup/BaseSetup.cs
@@ -1,104 +1,124 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using NHibernate;
 using SpeakFriend.Utilities;
 
 namespace SpeakFriend.Utilities
 {
-    public abstract class BaseSetup<TSubject, TDerivedClass> where TDerivedClass : BaseSetup<TSubject, TDerivedClass>
-    {
-        private readonly IDataService<TSubject> _dataService;
+	public abstract class BaseSetup<TSubject, TDerivedClass> where TDerivedClass : BaseSetup<TSubject, TDerivedClass>
+	{
+		private readonly ISession _session;
+		private readonly IDataService<TSubject> _dataService;
 
-        private readonly List<TSubject> _itemsToCreate = new List<TSubject>();
-        public TSubject LastAdded { get { return _itemsToCreate.Last(); } }
+		private readonly List<TSubject> _itemsToCreate = new List<TSubject>();
 
-        public List<TSubject> Added { get { return _itemsToCreate; } }
-        public List<TSubject> Created = new List<TSubject>();
-        public TSubject LastCreated { get { return Created.Last(); } }
+		protected BaseSetup(IDataService<TSubject> dataService)
+		{
+			_dataService = dataService;
+		}
 
-        protected BaseSetup(IDataService<TSubject> dataService)
-        {
-            _dataService = dataService;
-        }
+		protected BaseSetup(ISession session)
+		{
+			_session = session;
+		}
 
-        public TDerivedClass Add()
-        {
-            return Add(Get());
-        }
+		public TSubject LastAdded
+		{
+			get { return _itemsToCreate.Last(); }
+		}
 
-        public TDerivedClass Add(int amount)
-        {
-            for (int i = 0; i < amount; i++)
-                Add(Get());
+		public List<TSubject> Added
+		{
+			get { return _itemsToCreate; }
+		}
 
-            return (TDerivedClass)this;
-        }
+		public List<TSubject> Created = new List<TSubject>();
 
-        public TDerivedClass Add(TSubject subject)
-        {
-            _itemsToCreate.Add(subject);
+		public TSubject LastCreated
+		{
+			get { return Created.Last(); }
+		}
 
-            return (TDerivedClass)this;
-        }
+		public virtual TDerivedClass Add()
+		{
+			return Add(Get());
+		}
 
-        public abstract TSubject Get();
+		public virtual TDerivedClass Add(int amount)
+		{
+			for (var i = 0; i < amount; i++)
+				Add(Get());
 
-        public List<TSubject> Get(int amount)
-        {
-            var result = new List<TSubject>();
-			for (int i = 0; i < amount; i++)
+			return (TDerivedClass) this;
+		}
+
+		public virtual TDerivedClass Add(TSubject subject)
+		{
+			_itemsToCreate.Add(subject);
+
+			return (TDerivedClass) this;
+		}
+
+		public abstract TSubject Get();
+
+		public virtual List<TSubject> Get(int amount)
+		{
+			var result = new List<TSubject>();
+			for (var i = 0; i < amount; i++)
 			{
 				var item = Get();
 				result.Add(item);
 				Add(item);
 			}
 
-        	return result;
-        }
+			return result;
+		}
 
-        public TDerivedClass Persist()
-        {
-            foreach (var subject in _itemsToCreate)
-            {
-                _dataService.Create(subject);
-                Created.Add(subject);
-            }
+		public virtual TDerivedClass Persist()
+		{
+			foreach (var subject in _itemsToCreate)
+			{
+				if (_dataService != null)
+					_dataService.Create(subject);
+				else
+					_session.Save(subject);
+				Created.Add(subject);
+			}
 
-            _itemsToCreate.Clear();
+			_itemsToCreate.Clear();
 
-            return (TDerivedClass)this;
-        }
+			return (TDerivedClass) this;
+		}
 
-        public TSubject GetPersisted()
-        {
-            Add().Persist();
-            return Created.Last();
-        }
+		public virtual TSubject GetPersisted()
+		{
+			Add().Persist();
+			return Created.Last();
+		}
 
-        public TSubject GetPersisted(Func<TSubject, TSubject> modifier)
-        {
-            Add();
-            var subject = modifier(LastAdded);
-            Persist();
-            return subject;
-        }
+		public virtual TSubject GetPersisted(Func<TSubject, TSubject> modifier)
+		{
+			Add();
+			var subject = modifier(LastAdded);
+			Persist();
+			return subject;
+		}
 
-        public TSubject GetPersisted(Action<TSubject> modifier)
-        {
-            Add();
-            var subject = LastAdded;
-        	modifier(subject);
-            Persist();
-            return subject;
-        }
+		public virtual TSubject GetPersisted(Action<TSubject> modifier)
+		{
+			Add();
+			var subject = LastAdded;
+			modifier(subject);
+			Persist();
+			return subject;
+		}
 
-        public List<TSubject> GetPersisted(int amount)
-        {
-            var subjects = Get(amount);
-            Persist();
-            return subjects;
-        }
-    }
+		public virtual List<TSubject> GetPersisted(int amount)
+		{
+			var subjects = Get(amount);
+			Persist();
+			return subjects;
+		}
+	}
 }
--- SpeakLib.Utilities/Persistence/Interfaces/Mappings.cs
+++ SpeakLib.Utilities/Persistence/Interfaces/Mappings.cs
@@ -2,6 +2,10 @@
 
 namespace SpeakFriend.Utilities
 {
+	/// <summary>
+	/// Defines mapping for <see cref="PersistableBase.Id"/> and <see cref="PersistableBase.DateCreated"/>.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
 	public abstract class PersistableBaseMapping<T> : ClassMap<T>
 		where T : PersistableBase
 	{
@@ -12,6 +16,11 @@ namespace SpeakFriend.Utilities
 		}
 	}
 
+	/// <summary>
+	/// Defines mapping for <see cref="PersistableBase.Id"/>, <see cref="PersistableBase.DateCreated"/>
+	/// and <see cref="MutablePersistableBase.DateModified"/>.
+	/// </summary>
+	/// <typeparam name="T"></typeparam>
 	public abstract class MutablePersistableBaseMapping<T> : PersistableBaseMapping<T>
 		where T : MutablePersistableBase
 	{
