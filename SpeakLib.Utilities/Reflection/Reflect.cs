e20cf79e1784cbe8c5bf1dcb567d522bf699dd99
Oliver <oliver.friedrich@gmail.com>
2011-02-24 23:24:28 +0100

    new Reflect<T> class for type safe access to members

---
+++ SpeakLib.Utilities/Reflection/Reflect.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+using SpeakFriend.Utilities;
+
+namespace SpeakFriend.Utilities.Reflection
+{
+	/// <summary>
+	/// Get members of a class in a type safe manner.
+	/// </summary>
+	/// <remarks>
+	/// Inspired by http://www.nickbutler.net/Article/StrongNames
+	/// </remarks>
+	public static class Reflect<TClass>
+	{
+		public static string MemberName<TMember>(Expression<Func<TClass, TMember>> m)
+		{
+			return GetMemberInfo(m).Name;
+		}
+
+		public static FieldInfo Field<T>(Expression<Func<TClass, T>> m)
+		{
+			return GetFieldInfo(m);
+		}
+
+		public static PropertyInfo Property<T>(Expression<Func<TClass, T>> m)
+		{
+			return GetPropertyInfo(m);
+		}
+
+		public static MethodInfo Method(Expression<Action<TClass>> m)
+		{
+			return GetMethodInfo(m);
+		}
+
+		public static MethodInfo Method<T1>(Expression<Action<TClass, T1>> m)
+		{
+			return GetMethodInfo(m);
+		}
+
+		public static MethodInfo Method<T1, T2>(Expression<Action<TClass, T1, T2>> m)
+		{
+			return GetMethodInfo(m);
+		}
+
+		public static MethodInfo Method<T1, T2, T3>(Expression<Action<TClass, T1, T2, T3>> m)
+		{
+			return GetMethodInfo(m);
+		}
+
+		public static MethodInfo Method<T1, T2, T3, T4>(Expression<Action<TClass, T1, T2, T3, T4>> m)
+		{
+			return GetMethodInfo(m);
+		}
+
+		public static MethodInfo Method<T1, T2, T3, T4, T5>(Expression<Action<TClass, T1, T2, T3, T4, T5>> m)
+		{
+			return GetMethodInfo(m);
+		}
+
+		public static MethodInfo Method<T1, T2, T3, T4, T5, T6>(Expression<Action<TClass, T1, T2, T3, T4, T5, T6>> m)
+		{
+			return GetMethodInfo(m);
+		}
+
+		public static MethodInfo Method<T1, T2, T3, T4, T5, T6, T7>(Expression<Action<TClass, T1, T2, T3, T4, T5, T6, T7>> m)
+		{
+			return GetMethodInfo(m);
+		}
+
+		public static MethodInfo Method<T1, T2, T3, T4, T5, T6, T7, T8>(Expression<Action<TClass, T1, T2, T3, T4, T5, T6, T7, T8>> m)
+		{
+			return GetMethodInfo(m);
+		}
+
+		private static FieldInfo GetFieldInfo(LambdaExpression lambda)
+		{
+			return (FieldInfo) GetMemberInfo(lambda);
+		}
+
+		private static PropertyInfo GetPropertyInfo(LambdaExpression lambda)
+		{
+			return (PropertyInfo) GetMemberInfo(lambda);
+		}
+
+		private static MemberInfo GetMemberInfo(LambdaExpression lambda)
+		{
+			return ((MemberExpression) lambda.Body).Member;
+		}
+
+		private static MethodInfo GetMethodInfo(LambdaExpression lambda)
+		{
+			return ((MethodCallExpression) lambda.Body).Method;
+		}
+
+		public static class Static
+		{
+			public static string MemberName<TMember>(Expression<Func<TMember>> m)
+			{
+				return GetMemberInfo(m).Name;
+			}
+
+			public static FieldInfo Field<T>(Expression<Func<T>> m)
+			{
+				return GetFieldInfo(m);
+			}
+
+			public static PropertyInfo Property<T>(Expression<Func<T>> m)
+			{
+				return GetPropertyInfo(m);
+			}
+
+			public static MethodInfo Method(Expression<Action> m)
+			{
+				return GetMethodInfo(m);
+			}
+
+			public static MethodInfo Method<T1>(Expression<Action<T1>> m)
+			{
+				return GetMethodInfo(m);
+			}
+
+			public static MethodInfo Method<T1, T2>(Expression<Action<T1, T2>> m)
+			{
+				return GetMethodInfo(m);
+			}
+
+			public static MethodInfo Method<T1, T2, T3>(Expression<Action<T1, T2, T3>> m)
+			{
+				return GetMethodInfo(m);
+			}
+
+			public static MethodInfo Method<T1, T2, T3, T4>(Expression<Action<T1, T2, T3, T4>> m)
+			{
+				return GetMethodInfo(m);
+			}
+
+			public static MethodInfo Method<T1, T2, T3, T4, T5>(Expression<Action<T1, T2, T3, T4, T5>> m)
+			{
+				return GetMethodInfo(m);
+			}
+
+			public static MethodInfo Method<T1, T2, T3, T4, T5, T6>(Expression<Action<T1, T2, T3, T4, T5, T6>> m)
+			{
+				return GetMethodInfo(m);
+			}
+
+			public static MethodInfo Method<T1, T2, T3, T4, T5, T6, T7>(Expression<Action<T1, T2, T3, T4, T5, T6, T7>> m)
+			{
+				return GetMethodInfo(m);
+			}
+
+			public static MethodInfo Method<T1, T2, T3, T4, T5, T6, T7, T8>(Expression<Action<T1, T2, T3, T4, T5, T6, T7, T8>> m)
+			{
+				return GetMethodInfo(m);
+			}
+		}
+	}
+}
---
+++ Tests/Reflection/ReflectTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using SharpTestsEx;
+using SpeakFriend.Utilities.Reflection;
+
+namespace Tests.Reflection
+{
+	class Class
+	{
+		public static int StaticField = 42;
+		public static int StaticProperty { get; set; }
+		public static void StaticAction() { }
+		public static void StaticAction(int i) { }
+		public static int StaticFunc() { return 42; }
+		public static int StaticFunc(int i) { return i; }
+
+		public int Field = 42;
+		public int Property { get; set; }
+		public void Action() { }
+		public void Action(int i) { }
+		public int Func() { return 42; }
+		public int Func(int i) { return i; }
+	}
+
+	[TestFixture]
+	public class ReflectTests
+	{
+		[Test]
+		public void Instance_Field()
+		{
+			Reflect<Class>.Field(c => c.Field).Name.Should().Be.EqualTo("Field");
+		}
+
+		public void Instance_MemberName()
+		{
+			Reflect<Class>.MemberName(c => c.Field).Should().Be.EqualTo("Field");
+		}
+	}
+}
