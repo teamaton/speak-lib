402d0713f0277d3bfa6a2169044ba93706687524
Oliver <oliver.friedrich@gmail.com>
2012-06-25 10:56:16 +0200

    fix HoverImageButton to insert suffix before last dot (not first)

--- SpeakLib.Utilities/Web/View/WebForms/Controls/HoverImageButton.cs
+++ SpeakLib.Utilities/Web/View/WebForms/Controls/HoverImageButton.cs
@@ -1,34 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Web;
+﻿using System.ComponentModel;
 using System.Web.UI;
-using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 
 namespace SpeakFriend.Utilities
 {
-    [ToolboxData("<{0}:HoverImage runat=server></{0}:HoverImage>")]
-    public class HoverImageButton : ImageButton
-    {
-        public HoverImageButton()
-        {
-            HoverSuffix = "-on";
-        }
+	[ToolboxData("<{0}:HoverImage runat=server></{0}:HoverImage>")]
+	public class HoverImageButton : ImageButton
+	{
+		public HoverImageButton()
+		{
+			HoverSuffix = "-on";
+		}
 
-        [Bindable(false), Category("Appearance"), DefaultValue("-on"), Localizable(false)]
-        public string HoverSuffix { get; set; }
+		[Bindable(false), Category("Appearance"), DefaultValue("-on"), Localizable(false)]
+		public string HoverSuffix { get; set; }
 
-        protected override void Render(HtmlTextWriter writer)
-        {
-            var hoverSrc = ImageUrl.Insert(ImageUrl.IndexOf('.'), HoverSuffix);
+		protected override void Render(HtmlTextWriter writer)
+		{
+			var hoverSrc = ImageUrl.Insert(ImageUrl.LastIndexOf('.'), HoverSuffix);
 
-            Attributes.Add("onmouseover", string.Format("src='{0}'", hoverSrc));
-            Attributes.Add("onmouseout", string.Format("src='{0}'", ImageUrl));
-            
-            base.Render(writer);
-        }
-    }
+			Attributes.Add("onmouseover", string.Format("src='{0}'", hoverSrc));
+			Attributes.Add("onmouseout", string.Format("src='{0}'", ImageUrl));
+
+			base.Render(writer);
+		}
+	}
 }
